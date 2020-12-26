1332bd5eff240a0457bc89050e88b9395fbbbac1
Giorgi Gogichaishvili <gogichaishvilig544@gmail.com>
2020-12-26 22:51:57 +0400

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Giorgi Gogichaishvili on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
